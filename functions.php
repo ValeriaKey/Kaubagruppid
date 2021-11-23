cffd2fcef9f9356ad2a1ea74a018bc990574cfb7
ValeriaKey <valeriakolossova@outlook.com>
2021-11-23 11:25:16 +0300

    maakonna funktsioonide kustutamine

--- functions.php
+++ functions.php
@@ -1,31 +1,6 @@
 <?php
 require ('conf.php');
 
-function countyData($sort_by = "eesnimi", $search_term = "") {
-    global $connection;
-    $sort_list = array("eesnimi", "perekonnanimi", "maakonna_nimi");
-    if(!in_array($sort_by, $sort_list)) {
-        return "Seda tulpa ei saa sorteerida";
-    }
-    $request = $connection->prepare("SELECT inimene.id, eesnimi, perekonnanimi, maakond.maakonna_nimi
-    FROM inimene, maakond 
-    WHERE inimene.maakonna_id = maakond.id 
-    AND (eesnimi LIKE '%$search_term%' OR perekonnanimi LIKE '%$search_term%' OR maakonna_nimi LIKE '%$search_term%')
-    ORDER BY $sort_by");
-    $request->bind_result($id, $eesnimi, $perekonnanimi, $maakonna_nimi);
-    $request->execute();
-    $data = array();
-    while($request->fetch()) {
-        $person = new stdClass();
-        $person->id = $id;
-        $person->eesnimi = htmlspecialchars($eesnimi);
-        $person->perekonnanimi = htmlspecialchars($perekonnanimi);
-        $person->maakonna_nimi = $maakonna_nimi;
-        array_push($data, $person);
-    }
-    return $data;
-}
-
 function createSelect($query, $name) {
     global $connection;
     $query = $connection->prepare($query);
@@ -38,42 +13,6 @@ function createSelect($query, $name) {
     $result .= "</select>";
     return $result;
 }
-
-    function addCounty($county_name, $county_centre)
-    {
-        global $connection;
-        $query = $connection->prepare("INSERT INTO maakond (maakonna_nimi, maakonna_keskus)
-    VALUES (?, ?)");
-        $query->bind_param("ss", $county_name, $county_centre);
-        $query->execute();
-    }
-
-function addPerson($first_name, $last_name, $county_id) {
-
-
-    global $connection;
-    $query = $connection->prepare("INSERT INTO inimene (eesnimi, perekonnanimi, maakonna_id)
-    VALUES (?, ?, ?)");
-    $query->bind_param("ssd", $first_name, $last_name, $county_id);
-    $query->execute();
-}
-
-function deletePerson($person_id) {
-    global $connection;
-    $query = $connection->prepare("DELETE FROM inimene WHERE id=?");
-    $query->bind_param("i", $person_id);
-    $query->execute();
-}
-
-function savePerson($person_id, $first_name, $last_name, $county_id) {
-    global $connection;
-    $query = $connection->prepare("UPDATE inimene
-    SET eesnimi=?, perekonnanimi=?, maakonna_id=?
-    WHERE inimene.id=?");
-    $query->bind_param("ssii", $first_name, $last_name, $county_id, $person_id);
-    $query->execute();
-}
-
 /* Funktsioonid kauba ja kaubagrupi tabelitele */
 
 // kaubade otsing ja väjund
@@ -137,33 +76,7 @@ function saveProduct($product_id, $product_name, $product_price, $group_id) {
     $query->bind_param("ssii", $product_name, $product_price, $group_id, $product_id);
     $query->execute();
 }
-// maakonna kustutamine ( admin )
-function deleteCounty($maakonna_id) {
-    global $connection;
-    $query = $connection->prepare("DELETE FROM maakond WHERE id=?");
-    $query->bind_param("i", $maakonna_id);
-    $query->execute();
-}
-
-// maakondade tabeli kuvamiseks ( admin )
-function dataCounty() {
-    global $connection;
-    $request = $connection->prepare("SELECT id, maakonna_nimi,maakonna_keskus
-    FROM maakond");
-    $request->bind_result($id, $maakonna_nimi, $maakonna_keskus);
-    $request->execute();
-    $data = array();
-    while($request->fetch()) {
-        $maakond = new stdClass();
-        $maakond->id = $id;
-        $maakond->maakonna_nimi = htmlspecialchars($maakonna_nimi);
-        $maakond->maakonna_keskus = htmlspecialchars($maakonna_keskus);
-        array_push($data, $maakond);
-    }
-    return $data;
-} 
-
-// grupide tabeli kuvamiseks ( kasutaja )
+// grupide tabeli kuvamiseks ( admin )
 function dataGroups() {
     global $connection;
     $request = $connection->prepare("SELECT id, kaubagrupp
